9b3b2162d795361af54f5f2a58e88e787892aa8f
silviorsc <silvio.rogerio@gmail.com>
2021-05-04 18:54:02 -0300

    Add classe for Balance Service and Transaction Service

---
+++ transaction-service/src/main/java/com/moduscreate/bankaccount/TransactionOutgoingProducer.java
@@ -0,0 +1,28 @@
+package com.moduscreate.bankaccount;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
+
+@ApplicationScoped
+public class TransactionOutgoingProducer {
+
+    private final Logger log = Logger.getLogger(TransactionOutgoingProducer.class.getName());
+
+
+    /*
+     * Sempre que recebe uma trasaction envia uma mensagem para o servidor cluster do Kafka
+     */
+    @Incoming("transactions")
+    @Outgoing("transactions")
+    public KafkaRecord<String, Transaction> produce(Transaction transaction) {
+        log.info("Sending transaction with description " + transaction.description);
+        return KafkaRecord.of(UUID.randomUUID().toString(), transaction);
+    }
+    
+}
